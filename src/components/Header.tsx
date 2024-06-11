a77210535760711c2714a9a9ad2f610fe488f1f7
Carmen Diaz <karmenatwork@gmail.com>
2024-06-10 23:06:45 -0700

    Update Header component
    Add function to display nav for desktop layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,62 @@ import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 
+import { Popover, Transition } from "@headlessui/react";
+
+
 import clsx from "clsx";
 
 import { Fragment, useEffect, useRef, useState } from "react";
 
 import { Container } from "@/components/Container";
 import avatarImage from "@/images/avatar1.jpg";
+
+function NavItem({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  let isActive = usePathname() === href;
+
+  return (
+    <li>
+      <Link
+        href={href}
+        className={clsx(
+          "relative block px-3 py-2 transition",
+          isActive
+            ? "text-purple-800 dark:text-purple-700"
+            : "hover:text-purple-500 dark:hover:text-purple-400"
+        )}
+      >
+        {children}
+        {isActive && (
+          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-purple-500/0 via-purple-500/40 to-purple-500/0 dark:from-purple-400/0 dark:via-purple-400/40 dark:to-teal-400/0" />
+        )}
+      </Link>
+    </li>
+  );
+}
+
+function DesktopNavigation({ className, ...props }: React.ComponentPropsWithoutRef<"nav">) {
+  return (
+    <nav 
+      className={clsx(
+        className,
+        "hidden sm:flex place-items-start space-x-6"
+      )}
+    {...props}>
+      <ul className="flex ">
+        <NavItem href="/about">About</NavItem>
+        <NavItem href="/contact">Contact</NavItem>
+      </ul>
+    </nav>
+  );
+}
+
+
 
 function AvatarContainer({
   className,
@@ -107,7 +157,38 @@ export function Header() {
             </Container>
           </>
         )}
+        <div
+          ref={headerRef}
+          className="top-0 z-10 h-16 pt-6"
+          style={{
+            position:
+              "var(--header-position)" as React.CSSProperties["position"],
+          }}
+        >
+          <Container
+            className="top-[var(--header-top,theme(spacing.6))] w-full"
+            style={{
+              position:
+                "var(--header-inner-position)" as React.CSSProperties["position"],
+            }}
+          >
+            <div className="relative flex gap-4">
+              <div className="flex flex-none">
+                {!isHomePage && (
+                  <AvatarContainer>
+                    <Avatar />
+                  </AvatarContainer>
+                )}
+              </div>
+
+              <div className="flex flex-1 ">
+                <DesktopNavigation className="pointer-events-auto hidden md:block" />
+              </div>
+            </div>
+          </Container>
+        </div>
       </header>
+
       {isHomePage && (
         <div
           className="flex-none"
