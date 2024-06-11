3cecee049c6f8cc166883b5d9dace8022dad9a47
Carmen Diaz <karmenatwork@gmail.com>
2024-06-11 13:22:27 -0700

    Update Header component
    Add function to display nav for mobile layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,14 @@ import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 
-import { Popover, Transition } from "@headlessui/react";
+import {
+  Popover,
+  Transition,
+  TransitionChild,
+  PopoverButton,
+  PopoverPanel,
+  PopoverOverlay,
+} from "@headlessui/react";
 
 
 import clsx from "clsx";
@@ -12,6 +19,37 @@ import { Fragment, useEffect, useRef, useState } from "react";
 
 import { Container } from "@/components/Container";
 import avatarImage from "@/images/avatar1.jpg";
+
+// Icons
+function ChevronDownIcon(props: React.ComponentPropsWithoutRef<"svg">) {
+  return (
+    <svg viewBox="0 0 8 6" aria-hidden="true" {...props}>
+      <path
+        d="M1.75 1.75 4 4.25l2.25-2.5"
+        fill="none"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+
+  );
+}
+
+function CloseIcon(props: React.ComponentPropsWithoutRef<"svg">) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
 
 function NavItem({
   href,
@@ -55,6 +93,77 @@ function DesktopNavigation({ className, ...props }: React.ComponentPropsWithoutR
         <NavItem href="/contact">Contact</NavItem>
       </ul>
     </nav>
+  );
+}
+
+function MobileNavItem({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <li>
+      <PopoverButton as={Link} href={href} className="block py-2">
+        {children}
+      </PopoverButton>
+    </li>
+  );
+}
+
+function MobileNavigation(
+  props: React.ComponentPropsWithoutRef<typeof Popover>
+) {
+  return (
+    <Popover {...props}>
+      <PopoverButton className="group flex items-center rounded-none bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
+        Menu
+        <ChevronDownIcon className="ml-3 h-auto w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400" />
+      </PopoverButton>
+      <Transition>
+        <TransitionChild
+          as={Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="duration-150 ease-in"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <PopoverOverlay className="fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur-sm dark:bg-black/80" />
+        </TransitionChild>
+        <TransitionChild
+          as={Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-150 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <PopoverPanel
+            focus
+            className="fixed inset-x-4 top-8 z-50 origin-top rounded-none bg-white p-8 ring-1 ring-zinc-900/5 dark:bg-zinc-900 dark:ring-zinc-800"
+          >
+            <div className="flex flex-row-reverse items-center justify-between">
+              <PopoverButton aria-label="Close menu" className="-m-1 p-1">
+                <CloseIcon className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
+              </PopoverButton>
+              <h2 className="text-sm font-medium text-zinc-600 dark:text-zinc-400">
+                Navigation
+              </h2>
+            </div>
+            <nav className="mt-6">
+              <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+                <MobileNavItem href="/about">About</MobileNavItem>
+                <MobileNavItem href="/contact">Contact</MobileNavItem>
+              </ul>
+            </nav>
+          </PopoverPanel>
+        </TransitionChild>
+      </Transition>
+    </Popover>
   );
 }
 
@@ -182,6 +291,7 @@ export function Header() {
               </div>
 
               <div className="flex flex-1 ">
+                <MobileNavigation className="pointer-events-auto md:hidden" />
                 <DesktopNavigation className="pointer-events-auto hidden md:block" />
               </div>
             </div>
