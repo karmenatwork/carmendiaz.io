daa93513cb6f1bbd05bf79652170cb6b76ad1e77
Carmen Diaz <karmenatwork@gmail.com>
2024-06-10 20:38:35 -0700

    Add Header component
    It includes Avatar container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Header } from "@/components/Header";
 import { Container } from "@/components/Container";
 
 export default function Home() {
   return (
     <Container>
+      <Header />
       <div className="max-w-2xl text-base text-zinc-600 dark:text-zinc-400">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
           Hola! I’m Carmen 👋
