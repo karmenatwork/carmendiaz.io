1dc87e611f3b83e9edcf666d7cb91efbf56941be
Carmen Diaz <karmenatwork@gmail.com>
2024-06-11 19:50:13 -0700

    Wrap not-found page with Container component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,31 +1,33 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
- 
+import { Container } from "@/components/Container";
+
 export default function NotFound() {
   return (
     <>
-      <div className="mt-9">
-        <div className="max-w-2xl">
+      <Container>
+        <div className="max-w-2xl mt-9">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Sorry NOT FOUND 
+            Sorry NOT FOUND
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            Could not find requested resource | 
-            <Link href="/" className="text">  Return Home </Link>
+            Could not find requested resource |
+            <Link href="/" className="text">
+              {" "}
+              Return Home{" "}
+            </Link>
           </p>
           <Image
-        src={'/404page.jpeg'}
-        alt="image"
-        width={400}
-        height={100}
-        priority={true}
-        className="rounded-xl py-4 w-auto h-auto"
-    />
+            src={"/404page.jpeg"}
+            alt="image"
+            width={400}
+            height={100}
+            priority={true}
+            className="py-4 w-auto h-auto"
+          />
         </div>
-      </div>
+      </Container>
     </>
-
-
-  )
+  );
 }
