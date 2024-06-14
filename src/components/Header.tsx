19d594b68f0c861ee4014a40f078b02cb59bf6f8
Carmen Diaz <karmenatwork@gmail.com>
2024-06-13 17:43:47 -0700

    Update providers and Header component
    To include Dark / Light mode. Also, add home icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { useTheme } from "next-themes";
 
 import {
   Popover,
@@ -12,15 +13,109 @@ import {
   PopoverOverlay,
 } from "@headlessui/react";
 
-
 import clsx from "clsx";
 
 import { Fragment, useEffect, useRef, useState } from "react";
 
 import { Container } from "@/components/Container";
 import avatarImage from "@/images/avatar1.jpg";
+import homeIcon from "@/images/home.svg";
 
 // Icons
+
+function HomeIcon(props: React.ComponentPropsWithoutRef<"svg">) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="-1.44 -1.44 26.88 26.88"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M10.707 1.707a1 1 0 00-1.414 0l-7 7A1 1 0 003 10v7a1 1 0 001 1h4a1 1 0 001-1v-4h2v4a1 1 0 001 1h4a1 1 0 001-1v-7a1 1 0 00-.293-.707l-7-7z" />
+    </svg>
+  );
+}
+
+// function HomeIcon(props: React.ComponentPropsWithoutRef<"svg">) {
+//   return (
+//     <svg
+//       viewBox="0 0 8 6"
+//       aria-hidden="true"
+//       transform="matrix(-1, 0, 0, 1, 0, 0)rotate(0)"
+//       {...props}
+//     >
+//       {/* <path
+//         d="M1.75 1.75 4 4.25l2.25-2.5"
+//         fill="none"
+//         strokeWidth="1.5"
+//         strokeLinecap="round"
+//         strokeLinejoin="round"
+//       /> */}
+
+//       <g id="SVGRepo_bgCarrier" strokeWidth="0">
+//         <path
+//           transform="translate(-1.44, -1.44), scale(0.8399999999999999)"
+//           d="M16,31.574487284890242C20.08954739895304,31.64607442544839,24.119948136198598,29.888525923374377,26.790604396946875,26.790604396946875C29.303867765882018,23.87525628321231,29.74227054669274,19.836144877354048,29.4264543567385,16C29.139306082746092,12.512077545876792,27.706613196788844,9.23986724955995,25.149043933010965,6.850956066989037C22.67807327759347,4.542932775446971,19.3765422958824,3.281101673672767,16,3.458948850631714C12.784497704790043,3.6283138268695314,10.278592980095917,5.752164833142655,7.766517880229882,7.76651788022988C4.772629497570097,10.167221659576047,0.8068575495977862,12.188536944928577,0.36020817501204405,15.999999999999996C-0.10981477114888916,20.010920413780426,2.7191491372844596,23.569750661627026,5.580673439103254,26.419326560896742C8.435050834797329,29.261785396217572,11.97233668828964,31.503983419428234,16,31.574487284890242"
+//           fill="#c9a25d"
+//           strokeWidth="0"
+//         />
+//       </g>
+
+//       <g
+//         id="SVGRepo_tracerCarrier"
+//         strokeLinecap="round"
+//         strokeLinejoin="round"
+//       />
+
+//       <g id="SVGRepo_iconCarrier">
+//         {" "}
+//         <path
+//           d="M22 22L2 22"
+//           stroke="#691ea6"
+//           strokeWidth="1.5"
+//           strokeLinecap="round"
+//         />{" "}
+//         <path
+//           d="M2 11L6.06296 7.74968M22 11L13.8741 4.49931C12.7784 3.62279 11.2216 3.62279 10.1259 4.49931L9.34398 5.12486"
+//           stroke="#691ea6"
+//           strokeWidth="1.5"
+//           strokeLinecap="round"
+//         />{" "}
+//         <path
+//           d="M15.5 5.5V3.5C15.5 3.22386 15.7239 3 16 3H18.5C18.7761 3 19 3.22386 19 3.5V8.5"
+//           stroke="#691ea6"
+//           strokeWidth="1.5"
+//           strokeLinecap="round"
+//         />{" "}
+//         <path
+//           d="M4 22V9.5"
+//           stroke="#691ea6"
+//           strokeWidth="1.5"
+//           strokeLinecap="round"
+//         />{" "}
+//         <path
+//           d="M20 9.5V13.5M20 22V17.5"
+//           stroke="#691ea6"
+//           strokeWidth="1.5"
+//           strokeLinecap="round"
+//         />{" "}
+//         <path
+//           d="M15 22V17C15 15.5858 15 14.8787 14.5607 14.4393C14.1213 14 13.4142 14 12 14C10.5858 14 9.87868 14 9.43934 14.4393M9 22V17"
+//           stroke="#691ea6"
+//           strokeWidth="1.5"
+//           strokeLinecap="round"
+//           strokeLinejoin="round"
+//         />{" "}
+//         <path
+//           d="M14 9.5C14 10.6046 13.1046 11.5 12 11.5C10.8954 11.5 10 10.6046 10 9.5C10 8.39543 10.8954 7.5 12 7.5C13.1046 7.5 14 8.39543 14 9.5Z"
+//           stroke="#691ea6"
+//           strokeWidth="1.5"
+//         />{" "}
+//       </g>
+//     </svg>
+//   );
+// }
+
 function ChevronDownIcon(props: React.ComponentPropsWithoutRef<"svg">) {
   return (
     <svg viewBox="0 0 8 6" aria-hidden="true" {...props}>
@@ -32,7 +127,6 @@ function ChevronDownIcon(props: React.ComponentPropsWithoutRef<"svg">) {
         strokeLinejoin="round"
       />
     </svg>
-
   );
 }
 
@@ -51,6 +145,64 @@ function CloseIcon(props: React.ComponentPropsWithoutRef<"svg">) {
   );
 }
 
+// Dark / Light Mode icons - ThemeToggle
+function SunIcon(props: React.ComponentPropsWithoutRef<"svg">) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}
+    >
+      <path d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z" />
+      <path
+        d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061"
+        fill="none"
+      />
+    </svg>
+  );
+}
+
+function MoonIcon(props: React.ComponentPropsWithoutRef<"svg">) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        d="M17.25 16.22a6.937 6.937 0 0 1-9.47-9.47 7.451 7.451 0 1 0 9.47 9.47ZM12.75 7C17 7 17 2.75 17 2.75S17 7 21.25 7C17 7 17 11.25 17 11.25S17 7 12.75 7Z"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+function ThemeToggle() {
+  let { resolvedTheme, setTheme } = useTheme();
+  let otherTheme = resolvedTheme === "dark" ? "light" : "dark";
+  let [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <button
+      type="button"
+      aria-label={mounted ? `Switch to ${otherTheme} theme` : "Toggle theme"}
+      className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
+      onClick={() => setTheme(otherTheme)}
+    >
+      <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
+      <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500" />
+    </button>
+  );
+}
+
+// End Dark / Light Mode icons - ThemeToggle
+
+// Desktop Navigation
 function NavItem({
   href,
   children,
@@ -82,13 +234,11 @@ function NavItem({
 
 function DesktopNavigation({ className, ...props }: React.ComponentPropsWithoutRef<"nav">) {
   return (
-    <nav 
-      className={clsx(
-        className,
-        "hidden sm:flex place-items-start space-x-6"
-      )}
-    {...props}>
-      <ul className="flex ">
+    <nav
+      className={clsx(className, "hidden sm:flex place-items-start space-x-6")}
+      {...props}
+    >
+      <ul className="flex text-zinc-600 dark:text-zinc-200">
         <NavItem href="/about">About</NavItem>
         <NavItem href="/contact">Contact</NavItem>
       </ul>
@@ -96,6 +246,7 @@ function DesktopNavigation({ className, ...props }: React.ComponentPropsWithoutR
   );
 }
 
+// Mobile Navigation
 function MobileNavItem({
   href,
   children,
@@ -167,8 +318,25 @@ function MobileNavigation(
   );
 }
 
+// End Mobile Navigation
 
 
+function HomeContainer({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div
+      className={clsx(
+        className,
+        "h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10"
+      )}
+      {...props}
+    />
+  );
+}
+
+// Display my Avatar
 function AvatarContainer({
   className,
   ...props
@@ -282,17 +450,33 @@ export function Header() {
             }}
           >
             <div className="relative flex gap-4">
-              <div className="flex flex-none">
+              <div className="flex flex-0">
                 {!isHomePage && (
                   <AvatarContainer>
                     <Avatar />
                   </AvatarContainer>
                 )}
+                {isHomePage && (
+                  <HomeContainer>
+                    <Image
+                      src={homeIcon}
+                      alt="Home Page"
+                      sizes="2.25rem"
+                      className= "rounded-full bg-zinc-100 object-cover dark:bg-zinc-800 h-9 w-9"
+                      priority
+                    />
+                  </HomeContainer>
+                )}
               </div>
 
-              <div className="flex flex-1 ">
+              <div className="flex flex-1">
                 <MobileNavigation className="pointer-events-auto md:hidden" />
                 <DesktopNavigation className="pointer-events-auto hidden md:block" />
+              </div>
+              <div className="flex justify-end md:flex-1">
+                <div className="pointer-events-auto">
+                  <ThemeToggle />
+                </div>
               </div>
             </div>
           </Container>
