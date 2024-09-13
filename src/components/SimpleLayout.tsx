36116c4003139b3937c7442cc6a1a69b775c339d
Carmen Diaz <karmenatwork@gmail.com>
2024-09-13 12:57:04 -0700

    Add backgroup to the header

--- src/components/SimpleLayout.tsx
+++ src/components/SimpleLayout.tsx
@@ -1,4 +1,6 @@
 import { Container } from '@/components/Container'
+import Image from 'next/image'
+import carmenLaptop from '@/images/home/carmen-laptop.jpeg'
 
 export function SimpleLayout({
   title,
@@ -11,15 +13,27 @@ export function SimpleLayout({
 }) {
   return (
     <Container className="mt-16 sm:mt-32">
-      <header className="max-w-2xl">
-        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-          {title}
-        </h1>
-        <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-          {intro}
-        </p>
-      </header>
-      {children && <div className="mt-16 sm:mt-20">{children}</div>}
+      <section className="80vh relative flex justify-end">
+        <Image
+          src={carmenLaptop}
+          alt=""
+          sizes="100vw"
+          placeholder="blur"
+          style={{ width: '100%', height: 'auto' }}
+          className="rounded-2xl"
+        />
+
+        <header className="absolute z-10 pt-8 pe-8">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-100 sm:text-5xl dark:text-zinc-100">
+            {title}
+          </h1>
+          <p className="mt-2 max-w-96  text-base text-zinc-100 dark:text-zinc-100">
+            {intro}
+          </p>
+        </header>
+      </section>
+
+      <div>{children && <div className="mt-16 sm:mt-20">{children}</div>}</div>
     </Container>
   )
 }
