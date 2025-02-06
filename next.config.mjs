a0c5c3173d448801a1e5893e56683fe43cfdc927
Carmen Diaz <karmenatwork@gmail.com>
2025-02-06 14:39:23 -0800

    Revert "Modify next.config.js to ensure Next.js picks up the environment variables from Firebase."
    This reverts commit 1fbe99c679cb57c55b3532d0419c5b096cd97f5c.

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,12 @@
-import nextMDX from '@next/mdx'
+import nextMDX from "@next/mdx";
 import remarkGfm from 'remark-gfm'
-import rehypeExternalLinks from 'rehype-external-links'
+import rehypeExternalLinks from "rehype-external-links";
 
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
-      process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID:
-      process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET:
-      process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID:
-      process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  },
-}
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+};
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
   options: {
@@ -28,11 +16,11 @@ const withMDX = nextMDX({
         rehypeExternalLinks,
         {
           target: '_blank',
-          rel: ['noopener', 'noreferrer'],
-        },
-      ],
+          rel: ['noopener', 'noreferrer']
+        }
+      ]
     ],
-  },
+  }
 })
 
 export default withMDX(nextConfig)
